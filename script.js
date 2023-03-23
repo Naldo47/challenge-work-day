07ba5cb6383b0b7a19bb3ce151ecbb3baa1807d9
Naldo47 <reinaldo.santana444@gmail.com>
2023-03-23 11:02:00 -0400

    finished function and a README fule

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-<script src="path/to/dayjs/dayjs.min.js"></script>
 
 $(document).ready(function () {
 
@@ -31,7 +30,7 @@ localStorage.setItem(time, text);
 
 
 $("#hour-7 .description").val(localStorage.getItem("hour-7")); 
-$("#hour-8 .descritpion").val(localStorage.getItem("hour-8")); 
+$("#hour-8 .description").val(localStorage.getItem("hour-8")); 
 $("#hour-9 .description").val(localStorage.getItem("hour-9")); 
 $("#hour-10 .description").val(localStorage.getItem("hour-10")); 
 $("#hour-11 .description").val(localStorage.getItem("hour-11")); 
@@ -41,15 +40,13 @@ $("#hour-14 .description").val(localStorage.getItem("hour-14"));
 $("#hour-15 .description").val(localStorage.getItem("hour-15")); 
 
 
-
-
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  const saveBtn = document.querySelector ("saveBtn");
+  const saveBtn = document.querySelector (".saveBtn");
   saveBtn.addEventListener("click", function() {
     const userInput = this.parentNode.querySelector(".description").value;
     const hour = this.parentNode.id.split("-")[1];
@@ -70,12 +67,10 @@ $("#hour-15 .description").val(localStorage.getItem("hour-15"));
 if(hour < currentHour) {
 timeBlock.classList.add("past");
 timeBlock.classList.remove("present", "future");
-}
-else if (hour = currentHour) {
+} else if (hour = currentHour) {
 timeBlock.classList.add("present");
 timeBlock.classList.remove("past", "future");
-}
-else (hour > currentHour) {
+} else {
   timeBlock.classList.add("future");
   timeBlock.classList.remove("past", "present");
 }
@@ -95,14 +90,7 @@ if (saveData) {
       input.value = inputs[id];
     }
   }
-  
-}
-
-
-
-
-
-
+  }
 // TODO: Add code to display the current date in the header of the page.
 //Display current date
 const currentDate = new Date 
@@ -111,4 +99,4 @@ const header = document.querySelector("header");
 header.innerHTML = `<h1>${currentDate.toDateString()}</h1>`;
 
 
-
+})
